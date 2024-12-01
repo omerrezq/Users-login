cdd4a0443aa1e2b59908abe749b5972fbfeddddc
omerrezq <omerasraan@gmail.com>
2024-11-30 19:08:48 -0800

    Add files via upload

---
+++ users/js/main.js
@@ -0,0 +1,80 @@
+function show_add() {
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr["info"]("User added successfully", "User added")
+}
+function show_del() {
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr["error"]("User deleted successfully", "delete user")
+}
+function show_update() {
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr["success"]("User updates successfully", "Update user")
+}
+
+
+function cofirm_delete(id) {
+    let del = confirm("Do you want to delete the user ?")
+    console.log(del)
+    if (del == true) {
+        window.location.href = "index.php?action=del&&id=" + id;
+    }
+}
+function edit(id) {
+
+    window.location.href = "add_user.php?action=edit&&id=" + id;
+
+}
